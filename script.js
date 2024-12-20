a4bc3cef184061b2117d63fe63e4a425ea6f952b
am20241219 <am20241219@gmail.com>
2024-12-20 08:34:37 +0200

    improve responsiveness on tablets

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ dimmer.addEventListener("click", () => {
   sideNav.classList.add("hidden");
   dimmer.classList.remove("dimmed");
 });
+
+// Create a media condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+// Check if the media query is true
+if (mediaQuery.matches) {
+  sideNav.classList.remove("hidden");
+  dimmer.classList.remove("dimmed");
+}
